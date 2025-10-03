c07260a47b7c2ceb2b59c1aedba3fea9050466df
kurokoge396 <396robato@gmail.com>
2025-10-03 15:07:27 +0900

    WebAPIの呼び出しと表示まで

--- BlazorApp1/BlazorApp1/Program.cs
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -1,10 +1,19 @@
 using BlazorApp1.Components;
+using BlazorApp1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// webapi óp HttpClient Çìoò^
+builder.Services.AddHttpClient("MyApi", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7168/"); // Å© WebAPI ÇÃ URL
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<UserApiService>();
 
 var app = builder.Build();
 
---
+++ BlazorApp1/BlazorApp1/Services/UserApiService.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorApp1.Services
+{
+    public class UserApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserApiService(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient("MyApi");
+        }
+
+        public async Task<List<User?>> GetUserAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<User>>("api/Users");
+        }
+    }
+
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string name { get; set; } = null;
+
+        [JsonPropertyName("password")]
+        public string password { get; set; } = null;
+
+        [JsonPropertyName("email")]
+        public string email { get; set; } = null;
+    }
+}
